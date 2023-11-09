7baa2c2153a0f622f1103d52b8ccc3945b155ce9
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-11-09 16:02:06 +0000

    Add utils file containing two functions
    - Add function return sentiment from text
    - Add a function that uploads a file, runs the content of file through
    the get function and returns a sentiment of the text

---
+++ utils.py
@@ -0,0 +1,27 @@
+from nicegui import events, ui
+from PyPDF2 import PdfReader
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+def get_sentiment(text):
+    analyzer = SentimentIntensityAnalyzer()
+    vs = analyzer.polarity_scores(text)
+    if vs["compound"]  >= 0.05:
+       return "Postive"
+    elif vs["compound"] > -0.05 and vs["compound"] < 0.05:
+        return  "Neutral"
+    else:
+        return "Negative"
+    
+def handle_upload(event: events.UploadEventArguments):
+    with event.content as f:
+        # uploaded_file = Pim.open(f)
+        reader = PdfReader(f)
+        page = reader.pages[0]
+        text = page.extract_text()
+        with ui.element("div").classes("grid grid-cols-1 lg:grid-cols-2 gap-4 w-full"):
+            with ui.element("div").classes("shadow-lg p-4 w-full"):
+                ui.label(text).classes("w-full")
+            with ui.element("div"):
+                ui.label(get_sentiment(text)).classes("shadow-lg p-4 text-center h-full text-blue-400 text-5xl w-full")
+        
