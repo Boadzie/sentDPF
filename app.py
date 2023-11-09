48b9e985dfaea325dc1eea74fcfc6260ed4ea904
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-11-09 16:50:17 +0000

    Make file upload input responsive

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ with ui.element("div").classes("shadow-lg font-black mb-2 w-full p-4 rounded-md"
     ui.label("Upload a pdf file and get sentiment from the first page").classes("text-lg italic text-white")
 
 
-ui.upload(auto_upload=True, on_upload=handle_upload).props("accept=.pdf").classes("w-1/2 text-slate-500")
+ui.upload(auto_upload=True, on_upload=handle_upload).props("accept=.pdf").classes("w-full lg:w-1/2 text-slate-500")
 with ui.footer().classes("shadow-lg bg-[#03045e] mx-5 rounded-md"):
     ui.label("©2023 Daniel Boadzie")
 
