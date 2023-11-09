9050000f56582bd86817b47739760bff03460634
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-11-09 16:25:19 +0000

    Add footer to app
    - Change body background color
    - Add shadow to header and footer

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from nicegui import app, events, ui
 
 from utils import handle_upload
 
-with ui.element("div").classes("font-black mb-2 bg-slate-600 w-full p-4 rounded-md"):
-    ui.label("Sent_pdf").classes("text-4xl  text-blue-400")
-    ui.label("Upload a pdf file and get sentiment from the first line").classes("text-lg italic text-slate-400")
+ui.query('body').classes("bg-[#03045e]")
+ui.query('body').classes("text-white")
+with ui.element("div").classes("shadow-lg font-black mb-2  w-full p-4 rounded-md"):
+    ui.label("SentPDF").classes("text-4xl  text-blue-400")
+    ui.label("Upload a pdf file and get sentiment from the first line").classes("text-lg italic text-white")
 
 
 ui.upload(auto_upload=True, on_upload=handle_upload).props("accept=.pdf").classes("w-1/2")
-
+with ui.footer().classes("shadow-lg bg-[#03045e] mx-5 rounded-md"):
+    ui.label("©2023 Daniel Boadzie")
 
 ui.run()
